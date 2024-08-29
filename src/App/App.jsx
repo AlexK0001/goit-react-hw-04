9267a204cd89631084aab7ce3006d6d2686e9524
Alexandr <alexandr.kopyletz@gmail.com>
2024-08-29 23:47:17 +0300

    29.08

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+import {Formik, Form, Field} from 'formik'
 import './App.css'
+import axios from 'axios'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+
+  const [image, setImage] = useState(null);
+
+  const handleSearch = async (values, actions) => {
+
+  const response = avait axios.get(`https://api.unsplash.com/photos/?client_id=PvjMb00TBZQMjBYGyHn64d4ju2tX37ge2hlt-_SJJqA&query=${request}`);
+    console.log(response);
+
+    setImage(response.data);
+  };
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Formik initialValues={{image_name: ""}} onSubmit={handleSearch}>
+        <Form>
+          <Field type="text" name="image_name"></Field>
+          <button type="submit">Search</button>
+        </Form>
+      </Formik>
+    </div>
   )
 }
-
-export default App
